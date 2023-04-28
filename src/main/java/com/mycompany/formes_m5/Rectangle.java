a7d921890f17410219a1cde08087fbbd466ca258
Sergi <sefu928@vidalibarraquer.net>
2023-04-28 12:10:24 +0200

    first commit

---
+++ src/main/java/com/mycompany/formes_m5/Rectangle.java
@@ -0,0 +1,26 @@
+package com.mycompany.formes_m5;
+
+public class Rectangle {
+    private double longitud;
+    private double amplada;
+
+    public Rectangle(double longitud, double amplada) {
+        this.longitud = longitud;
+        this.amplada = amplada;
+    }
+    
+    /*
+    Responsable = Joan
+    public double getArea() {
+    }
+
+    Responsable = Edgar
+    public double getPerimeter() {
+    }
+    
+    Responsable = Sergi
+    public boolean esQuadrat() {
+    }
+    */
+}
+
